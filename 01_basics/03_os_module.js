f8937d8c6d4a3f098162d8e6adc49b179b9bea4c
Saurav-Mistri <sauravmistri21@gmail.com>
2025-02-16 19:23:25 +0530

    os module in js done

---
+++ 01_basics/03_os_module.js
@@ -0,0 +1,14 @@
+const os = require('os');
+
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.release());
+console.log(os.release());
+console.log(os.totalmem());
+console.log(os.tmpdir());
+console.log(os.type());
+console.log(os.userInfo());
+console.log(os.EOL);
+console.log(os.availableParallelism());
