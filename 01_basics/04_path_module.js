ba940b4221acc48b9283fc34887573a3d4c01a1a
Saurav-Mistri <sauravmistri21@gmail.com>
2025-02-16 19:30:39 +0530

    path modeule done

---
+++ 01_basics/04_path_module.js
@@ -0,0 +1,10 @@
+const path = require('node:path');
+
+const a1 = path.basename('C:\\temp\\myfile.html');
+const a2 = path.dirname('C:\\temp\\myfile.html');
+const a3 = path.extname(__filename);
+
+console.log(a1);
+console.log(a2);
+console.log(__filename);
+console.log(a3);
